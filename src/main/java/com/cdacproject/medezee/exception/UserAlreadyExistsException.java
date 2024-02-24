920cef12beedca920ce9c20f61758213f9b340cb
shubham sudhakar jadhav <shubhamjadhav4899@gmail.com>
2024-02-24 22:31:56 +0530

    Remove All File

--- src/main/java/com/cdacproject/medezee/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.cdacproject.medezee.exception;
-
-public class UserAlreadyExistsException extends RuntimeException{
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
