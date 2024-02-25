d69aa113b6deba37ba48d392d9041e2d9f072c82
shubham sudhakar jadhav <shubhamjadhav4899@gmail.com>
2024-02-25 22:36:26 +0530

    commit v2

---
+++ src/main/java/com/onlineshopping/dto/UserResponse.java
@@ -0,0 +1,20 @@
+package com.onlineshopping.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.onlineshopping.model.User;
+
+public class UserResponse extends CommonApiResponse {
+
+	private List<User> users = new ArrayList<>();
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+
+}
